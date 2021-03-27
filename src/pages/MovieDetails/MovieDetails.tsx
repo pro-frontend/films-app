4a64afb3aef2043d348fe6a68d82b62fb00fe169
pro-frontend <dzmitryj.ivchenkov@gmail.com>
2021-03-27 20:59:35 +0300

    fix(hotfix): modal types

--- src/pages/MovieDetails/MovieDetails.tsx
+++ src/pages/MovieDetails/MovieDetails.tsx
@@ -7,18 +7,20 @@ import { MovieInfo } from "../../components/MovieInfo";
 import { MoviesList } from "../../components/MoviesList";
 import { Paragraph, WrapperLarge } from "../../components/_StyledComponents";
 
-const MovieDetails = () => (
-  <>
-    <WrapperLarge className="content_minus_footer_height">
-      <Header />
-      <MovieInfo />
-      <Paragraph align="center">39 movies found</Paragraph>
-      <MoviesList />
-    </WrapperLarge>
-    <Footer />
-    <Modal filmId={0} active={false} mode={Tmode.CREATE} />
-    <Modal filmId={0} active={false} mode={Tmode.EDIT} />
-    <Modal filmId={0} active={false} mode={Tmode.DELETE} />
-  </>
-);
+const MovieDetails = () => {
+  return (
+    <>
+      <WrapperLarge className="content_minus_footer_height">
+        <Header />
+        <MovieInfo />
+        <Paragraph align="center">39 movies found</Paragraph>
+        <MoviesList />
+      </WrapperLarge>
+      <Footer />
+      <Modal filmId={0} active={false} mode={Tmode.CREATE} />
+      <Modal filmId={0} active={false} mode={Tmode.EDIT} />
+      <Modal filmId={0} active={false} mode={Tmode.DELETE} />
+    </>
+  );
+};
 export { MovieDetails };
