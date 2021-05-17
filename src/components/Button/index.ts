6f33b26a4dba326cc503c61220d7cf90f9eda967
pro-frontend <dzmitryj.ivchenkov@gmail.com>
2021-05-17 20:16:43 +0300

    fix(hot): add missed Button components

---
+++ src/components/Button/index.ts
@@ -0,0 +1,12 @@
+export {
+  AddMovieButton,
+  SearchMovieButton,
+  StyledButton as Button,
+  StyledLink as Link,
+} from "./Button";
+export {
+  ModalCreateConfirmButton,
+  ModalEditConfirmButton,
+  ModalRemoveConfirmButton,
+  ModalResetButton,
+} from "./ModalButton";
